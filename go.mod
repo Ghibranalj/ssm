aeede5a35702a2538cf3b50b9b260070c3f7b264
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2022-08-02 22:06:58 +0200

    feat: add enpoint generator & email templating

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ghibranalj/stupidsimplemailer
 go 1.18
 
 require (
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+	"strings"
+	"text/template"
 
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"gopkg.in/yaml.v3"
 )
@@ -19,7 +24,7 @@ type Service struct {
 	Bc           []string `yaml:"cc"`
 	Bcc          []string `yaml:"bcc"`
 	BodyFormat   string   `yaml:"bodyFormat"`
-	Cors         string   `yaml:"cors"`
+	Cors         []string `yaml:"cors"`
 }
 
 type Config struct {
@@ -32,7 +37,10 @@ type cliOpts struct {
 	envFile    string
 }
 
-var opts cliOpts
+var (
+	opts   cliOpts
+	router *mux.Router
+)
 
 func main() {
 	file, err := ioutil.ReadFile(opts.configFile)
@@ -52,6 +60,19 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Error loading .env file: %v\n", err)
 		os.Exit(1)
 	}
+
+	router = mux.NewRouter()
+	for _, service := range config.Services {
+		GenerateEndpoint(service)
+	}
+	http.Handle("/", router)
+
+	port := config.Port
+	err = http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Server error: %v\n", err)
+		os.Exit(1)
+	}
 }
 func init() {
 	confFile := flag.String("c", "config.yml", "config file")
@@ -60,4 +81,48 @@ func init() {
 	flag.Parse()
 	opts.configFile = *confFile
 	opts.envFile = *envFile
+}
+
+func GenerateEndpoint(service Service) {
+
+	endpoint := service.Endpoint
+	corsString := strings.Join(service.Cors, ",")
+
+	// TODO get the body format from the config & get the field names
+
+	bodyTemplate := template.New("body")
+	bodyTemplate, err := bodyTemplate.Parse(service.BodyFormat)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error parsing body format in service %s: %v\n", service.Endpoint, err)
+		os.Exit(1)
+	}
+
+	router.HandleFunc(endpoint, func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", corsString)
+
+		body := json.NewDecoder(r.Body)
+
+		var data map[string]interface{}
+		err := body.Decode(&data)
+		if err != nil {
+			log.Printf("Error parsing body: %v\n", err)
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "Error parsing body: %s\n", err.Error())
+			return
+		}
+		log.Printf("Received request: %v\n", data)
+
+		sb := &strings.Builder{}
+		err = bodyTemplate.Execute(sb, data)
+		if err != nil {
+			log.Printf("Error executing template: %v\n", err)
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "Error executing template: %s\n", err.Error())
+			return
+		}
+		log.Printf("Sending email: %s\n", sb.String())
+	}).Methods("POST")
+
+	log.Printf("Endpoint %s created succesfully\n", endpoint)
 }
